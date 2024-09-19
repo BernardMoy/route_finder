de781d3afc98fe357dc3c6a98011cfe71c557ef9
Bernard Moy <bernardmoy3@gmail.com>
2024-09-20 02:18:08 +0800

    Station input dialog

--- route_finder.py
+++ route_finder.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import pandas as pd
+import sys
 
 G = nx.MultiGraph()
 
@@ -15,7 +16,11 @@ G.add_nodes_from(stations_list)
 
 # Add the station codes to the dict
 code_to_station = pd.Series(df.Station_name.values, index=df.Station_code).to_dict()
-print(code_to_station)
+station_to_code = pd.Series(df.Station_code.values, index=df.Station_name).to_dict()
+station_lower_to_code = pd.Series(df.Station_code.values, index=df.Station_name.str.lower()).to_dict()
+stations_set = set([key for key, _ in station_to_code.items()])
+
+code_to_line = pd.Series(df.Line_name.values, index=df.Line_code).to_dict()
 
 
 # Keep track of the previous line and previous station
@@ -51,10 +56,50 @@ print("Graph edges with attributes:", G.edges(data=True))
 def shortest(code_start : str, code_end : str) -> str:
     return ""
 
-# Ask input for the start and end stations
+# Ask input for the start station
 print("========================================")
+
 code_start = input("Enter start station name / code:")
+
+while code_start.upper() not in code_to_station and code_start.lower() not in station_lower_to_code:
+    print("**Invalid start station provided!")
+    code_start = input("Enter start station name / code:")
+
+# Station code entered
+if code_start.upper() in code_to_station:
+    entered_code = code_start.upper()
+    print(f"Start station: {code_to_station[entered_code]} ({entered_code})")
+
+# Station name entered
+else:
+    code_start = station_lower_to_code[code_start.lower()]
+    entered_name = code_to_station[code_start]
+    print(f"Start station: {entered_name} ({code_start})")
+    
+# Ask input for end station
+print("========================================")
 code_end = input("Enter end station name / code:")
+
+while code_end.upper() not in code_to_station and code_end.lower() not in station_lower_to_code:
+    print("**Invalid end station provided!")
+    code_end = input("Enter end station name / code:")
+
+# Station code entered
+if code_end.upper() in code_to_station:
+    entered_code = code_end.upper()
+    print(f"End station: {code_to_station[entered_code]} ({entered_code})")
+
+# Station name entered
+else:
+    code_end = station_lower_to_code[code_end.lower()]
+    entered_name = code_to_station[code_end]
+    print(f"End station: {entered_name} ({code_end})")
+
+
+# Start and end stations cannot be the same
+if (code_start == code_end):
+    print("**Start and end station cannot be the same!")
+    sys.exit(1)
 
 
 # Draw the graph
